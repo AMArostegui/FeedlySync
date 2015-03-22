5252bf1249ab7783bcfe368ff0afc03465f3f22a
Antonio Miras <amarostegui@outlook.es>
2015-03-22 11:50:09 +0100

    -

--- src/fsprefs.js
+++ src/fsprefs.js
@@ -2,7 +2,6 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 
 const PREF_BRANCH = "extensions.FeedlySync.";
 const PREFS = {
-	// Global preferences
 	locale : Services.locale.getLocaleComponentForUserAgent(),
 	baseUrl : "http://sandbox.feedly.com",
 	baseSslUrl : "https://sandbox.feedly.com",
