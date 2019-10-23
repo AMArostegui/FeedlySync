cd91f60f183bff096f059a06db334b668e4ef8b4
Antonio Miras <AMArostegui@outlook.es>
2019-10-23 23:06:28 +0200

    v2.0.0. Working on compatibility with Thunderbird 68 - options.xul showing again

---
+++ content/preferences.js
@@ -0,0 +1,7 @@
+Preferences.addAll([
+    { id: "extensions.FeedlySync.synch.direction", type: "int" },
+    { id: "extensions.FeedlySync.synch.account", type: "string" },
+    { id: "extensions.FeedlySync.log.active", type: "bool" },
+    { id: "extensions.FeedlySync.log.toFile", type: "bool" },
+    { id: "extensions.FeedlySync.synch.timeout", type: "int" }
+]);
